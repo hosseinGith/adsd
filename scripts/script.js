26ab7c8c573a8a3dacb86e9a831189927667be93
hossein derees <hosseinderees7@gmail.com>
2023-09-16 07:49:03 -0700

    *

--- scripts/script.js
+++
@@ -1,9 +0,0 @@
-const container = document.querySelector(".container");
-
-window.addEventListener("touchmove", (e) => {
-  let x, y;
-  x = e.touches[0].clientX;
-  y = e.touches[0].clientY;
-  container.style.transform = `rotate3d(${y}, ${x}, ${y},${x}deg)`;
-  alert("true")
-  });
