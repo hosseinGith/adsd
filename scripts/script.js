2fb1c1a43fe30928992482a59c6c3447bb1fa6d5
hosseinGithub <133234861+hosseinGith@users.noreply.github.com>
2023-09-16 19:53:17 +0330

    Update script.js

--- scripts/script.js
+++ scripts/script.js
@@ -4,7 +4,11 @@ if (navigator.platform === "Win32") {
   window.addEventListener("mousemove", (e) => {
     let x = e.clientX - container.clientHeight;
     let y = e.clientY - container.clientWidth;
-    container.style.transform = `rotate3d(${y}, ${x}, ${y},${x}deg)`;
+    let xOffest += x;
+    let xOffest += y;
+    xOffest > 100? -xOffset : xOffset;
+    yOffest > 100? -yOffset : yOffset;
+    container.style.transform = `rotate3d(${yOffest}, ${xOffest}, ${yOffest},${xOffest}deg)`;
   });
 }
 
