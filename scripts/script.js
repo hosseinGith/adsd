d9c482e629dc71a167ed2bec57625e7f7af51105
hosseinGithub <133234861+hosseinGith@users.noreply.github.com>
2023-09-16 21:10:53 +0330

    Update script.js

--- scripts/script.js
+++ scripts/script.js
@@ -1,7 +1,8 @@
 const container = document.querySelector(".container");
 
   window.addEventListener("touchmove", (e) => {
-    let x += 50;
-    let y += 50;
+    let x, y;
+    x += 50;
+    y += 50;
     container.style.transform = `rotate3d(${y}, ${x}, ${y},${x}deg)`;
   });
